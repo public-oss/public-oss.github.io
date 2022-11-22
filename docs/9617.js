bae2dfb52f2adbfe3f485fbb6df058da3b2fe5ba
GitHub Action <action@github.com>
2022-11-22 03:47:07 +0000

    Auto update github page

---
+++ docs/9617.js
@@ -0,0 +1,2 @@
+/*! For license information please see 9617.js.LICENSE.txt */
+"use strict";(self.webpackChunk_public_oss_th_ik_ja_belegerfassung=self.webpackChunk_public_oss_th_ik_ja_belegerfassung||[]).push([[9617],{9617:(s,e,_)=>{_.r(e),_.d(e,{lean_test_closed:()=>n});var t=_(4576);const n=class{constructor(s){(0,t.r)(this,s),this._name=void 0}render(){return(0,t.h)("lean-test-open",{_name:this._name},(0,t.h)("slot",null))}}}}]);
