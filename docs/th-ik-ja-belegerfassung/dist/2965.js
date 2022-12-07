8bbdea946e6ccf12496f9ab8b838d795c5e7dc5d
GitHub Action <action@github.com>
2022-12-07 05:59:24 +0000

    Auto update github page

---
+++ docs/th-ik-ja-belegerfassung/dist/2965.js
@@ -0,0 +1,2 @@
+/*! For license information please see 2965.js.LICENSE.txt */
+"use strict";(self.webpackChunk_public_oss_th_ik_ja_belegerfassung=self.webpackChunk_public_oss_th_ik_ja_belegerfassung||[]).push([[2965],{2965:(s,e,l)=>{l.r(e),l.d(e,{kol_button_group_wc:()=>t});var r=l(8207);const t=class{constructor(s){(0,r.r)(this,s),this.state={}}render(){return(0,r.h)(r.H,null,(0,r.h)("div",{class:"flex flex-wrap bg-normal"},(0,r.h)("slot",null)))}}}}]);
