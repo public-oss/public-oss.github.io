1d775444a9a2d64cbf63211e716af180721a278d
GitHub Action <action@github.com>
2022-12-17 06:08:12 +0000

    Auto update github page

---
+++ docs/th-ik-ja-belegerfassung/1570.js
@@ -0,0 +1,2 @@
+/*! For license information please see 1570.js.LICENSE.txt */
+"use strict";(self.webpackChunk_public_oss_th_ik_ja_belegerfassung=self.webpackChunk_public_oss_th_ik_ja_belegerfassung||[]).push([[1570],{1570:(s,e,l)=>{l.r(e),l.d(e,{kol_button_group_wc:()=>t});var r=l(7501);const t=class{constructor(s){(0,r.r)(this,s),this.state={}}render(){return(0,r.h)(r.H,null,(0,r.h)("div",{class:"flex flex-wrap bg-normal"},(0,r.h)("slot",null)))}}}}]);
--- docs/th-ik-ja-belegerfassung/1915.js
+++
@@ -1,2 +0,0 @@
-/*! For license information please see 1915.js.LICENSE.txt */
-"use strict";(self.webpackChunk_public_oss_th_ik_ja_belegerfassung=self.webpackChunk_public_oss_th_ik_ja_belegerfassung||[]).push([[1915],{1915:(s,e,l)=>{l.r(e),l.d(e,{kol_button_group_wc:()=>t});var r=l(7227);const t=class{constructor(s){(0,r.r)(this,s),this.state={}}render(){return(0,r.h)(r.H,null,(0,r.h)("div",{class:"flex flex-wrap bg-normal"},(0,r.h)("slot",null)))}}}}]);
