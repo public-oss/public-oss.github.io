5e14c4084a6d81a3100a32518c5087129bf7ea48
GitHub Action <action@github.com>
2022-12-20 17:10:34 +0000

    Auto update github page

--- docs/th-ik-ja-belegerfassung/6586.js
+++
@@ -1,2 +0,0 @@
-/*! For license information please see 6586.js.LICENSE.txt */
-"use strict";(self.webpackChunk_public_oss_th_ik_ja_belegerfassung=self.webpackChunk_public_oss_th_ik_ja_belegerfassung||[]).push([[6586],{6586:(s,e,_)=>{_.r(e),_.d(e,{lean_test_closed:()=>n});var t=_(7428);const n=class{constructor(s){(0,t.r)(this,s),this._name=void 0}render(){return(0,t.h)("lean-test-open",{_name:this._name},(0,t.h)("slot",null))}}}}]);
