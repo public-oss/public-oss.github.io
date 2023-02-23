13cd4c62c53f7242a45f1090d4b2a3a680670002
GitHub Action <action@github.com>
2023-02-23 20:21:27 +0000

    Auto update github page

--- docs/th-ik-ja-belegerfassung/9814.js
+++
@@ -1,2 +0,0 @@
-/*! For license information please see 9814.js.LICENSE.txt */
-"use strict";(self.webpackChunk_public_oss_th_ik_ja_belegerfassung=self.webpackChunk_public_oss_th_ik_ja_belegerfassung||[]).push([[9814],{9814:(s,e,l)=>{l.r(e),l.d(e,{kol_button_group_wc:()=>t});var r=l(5259);const t=class{constructor(s){(0,r.r)(this,s),this.state={}}render(){return(0,r.h)(r.H,null,(0,r.h)("div",{class:"flex flex-wrap bg-normal"},(0,r.h)("slot",null)))}}}}]);
